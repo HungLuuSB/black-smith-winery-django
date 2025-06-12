e04f21bfbd13245a1e35e3e8db4e009c04a96b00
HungLuuSB <hungluu344@gmai.com>
2025-06-12 12:58:42 +0700

    Populate test data for category, brand, country

--- common/models/country.py
+++ common/models/country.py
@@ -1,4 +1,6 @@
 from django.db import models
 
+
 class Country(models.Model):
-    name = models.CharField(max_length=50,unique=True)
+    name = models.CharField(max_length=50, unique=True)
+    code = models.CharField(max_length=3, unique=True, null=True, blank=True)
