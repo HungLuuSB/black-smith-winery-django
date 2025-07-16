5bab8db4821bcd9f01192e36a17528c2459c9404
HungLuuSB <hungluu344@gmai.com>
2025-07-16 23:07:45 +0700

    Updated dashboard/admin

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+# dashboard/admin -> views.admin_index
 urlpatterns = [
-    path('', views.index, name='dashboard/index'),
-    path('AddProduct', views.add_product, name='dashboard/add_new_product')
+    path("", views.index, name="dashboard/index"),
+    path("admin", views.admin_index, name="dashboard/admin"),
+    path("AddProduct", views.add_product, name="dashboard/add_new_product"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,35 +2,42 @@ from django.shortcuts import render
 from dashboard.forms import AddNewProductForm
 from shop.models import Stock
 from common.models import Country, Brand, Category
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'dashboard/index.html')
+    return render(request, "dashboard/index.html")
+
+
+def admin_index(request):
+    return render(request, "dashboard/admin.html")
+
 
 def add_product(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AddNewProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save(commit=False)
-            country_name = request.POST.get('country')
+            country_name = request.POST.get("country")
             country, created = Country.objects.get_or_create(name=country_name)
             product.country = country
 
-            brand_name = request.POST.get('brand')
+            brand_name = request.POST.get("brand")
             brand, created = Brand.objects.get_or_create(name=brand_name)
-            
+
             product.brand = brand
             product.save()
-            quantity = form.cleaned_data['stock_quantity']
+            quantity = form.cleaned_data["stock_quantity"]
             Stock.objects.create(product=product, quantity=quantity)
-            return render(request, 'dashboard/index.html')
+            return render(request, "dashboard/index.html")
         else:
             print("Form errors:", form.errors)
             print("Non-field errors:", form.non_field_errors())
 
     context = {
-        'form': AddNewProductForm,
-        'countries': Country.objects.all(),
-        'brands': Brand.objects.all(),
-        'categories': Category.objects.all()
+        "form": AddNewProductForm,
+        "countries": Country.objects.all(),
+        "brands": Brand.objects.all(),
+        "categories": Category.objects.all(),
     }
-    return render(request, 'dashboard/add_new_product.html', context)
+    return render(request, "dashboard/add_new_product.html", context)
